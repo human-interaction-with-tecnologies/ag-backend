9b344bbaa4286915c5e857b792fe77af7cc1f90e
quaresmateo <teoian.giovane@gmail.com>
2022-05-15 19:10:05 -0300

    feat: creates professional domain
    \

---
+++ src/professional/domain/ports/professional.repository.ts
@@ -0,0 +1,16 @@
+import { Professional } from "../model/professional.model";
+
+
+export interface ProfessionalRepository {
+  create(professional: Professional): Promise<Professional>;
+
+  findAll(): Promise<Professional[]>;
+
+  findById(id: string): Promise<Professional>;
+
+  update(id: string, professional: Professional): Promise<Professional>;
+
+  delete(id: string): Promise<Professional>;
+}
+
+export const ProfessionalRepository = Symbol('ProfessionalRepository');
