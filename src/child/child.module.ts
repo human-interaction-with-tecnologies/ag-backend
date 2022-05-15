10589c3998ba00ba196c8c2b41476d892d491cae
quaresmateo <teoian.giovane@gmail.com>
2022-05-14 22:29:42 -0300

    fix: fixes some bugs

--- src/child/child.module.ts
+++ src/child/child.module.ts
@@ -3,12 +3,14 @@ import { ChildController } from './adapters/api/child.controller';
 import { ChildInMemory } from './adapters/db/child-in-memory.repository';
 import { ChildService } from './domain/ports/child.service';
 import { ChildRepository } from './domain/ports/child.repository';
+import { ChildApiService } from './adapters/api/child-api.service';
 
 @Module({
   controllers: [ChildController],
 
   providers: [
     ChildService,
+    ChildApiService,
     {
       provide: ChildRepository,
       useClass: ChildInMemory,
